29e2bb62173a148b61187734052e9a5880a16152
CarQG <cquijanogarcia.402@gmail.com>
2022-03-31 23:13:55 -0600

    primer commit

---
+++ src/main/java/com/example/inicioprac/vistas/Login.java
@@ -0,0 +1,16 @@
+package com.example.inicioprac.vistas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.inicioprac.R;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
